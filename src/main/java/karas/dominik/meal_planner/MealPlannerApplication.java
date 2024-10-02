1b6a87f8c1d57ae47d9efc4e7eab736ceffd08b9
Dominik <dominik.karas@clurgo.com>
2024-10-02 19:51:36 +0200

    Initial commit

---
+++ src/main/java/karas/dominik/meal_planner/MealPlannerApplication.java
@@ -0,0 +1,13 @@
+package karas.dominik.meal_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealPlannerApplication.class, args);
+	}
+
+}
