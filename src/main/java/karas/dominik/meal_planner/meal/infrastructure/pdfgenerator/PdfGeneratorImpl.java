4f58511cff2dc7a06d45868f2a3331894be06b1d
Dominik <dominik.karas@clurgo.com>
2024-10-15 22:38:34 +0200

    Improved PDF generation

--- src/main/java/karas/dominik/meal_planner/meal/infrastructure/pdfgenerator/PdfGeneratorImpl.java
+++ src/main/java/karas/dominik/meal_planner/meal/infrastructure/pdfgenerator/PdfGeneratorImpl.java
@@ -1,11 +1,12 @@
 package karas.dominik.meal_planner.meal.infrastructure.pdfgenerator;
 
-import com.itextpdf.text.BaseColor;
-import com.itextpdf.text.Chunk;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Font;
-import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 import karas.dominik.meal_planner.common.DayOfTheWeek;
 import karas.dominik.meal_planner.meal.domain.GenerateWeeklyPdfQuery;
@@ -16,9 +17,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayOutputStream;
+import java.util.Comparator;
 import java.util.Map;
 import java.util.Optional;
 
+import static com.itextpdf.text.Font.FontFamily.HELVETICA;
 import static karas.dominik.meal_planner.common.DayOfTheWeek.FRIDAY;
 import static karas.dominik.meal_planner.common.DayOfTheWeek.MONDAY;
 import static karas.dominik.meal_planner.common.DayOfTheWeek.SATURDAY;
@@ -42,14 +45,13 @@ class PdfGeneratorImpl implements PdfGenerator {
 
             var byteArrayOutputStream = new ByteArrayOutputStream();
 
-            var document = new Document();
+            var document = new Document(PageSize.A4.rotate());
             PdfWriter.getInstance(document, byteArrayOutputStream);
             document.open();
 
-            var font = FontFactory.getFont(FontFactory.COURIER, 16, BaseColor.BLACK);
-            var chunk = new Chunk("Hello World", font);
+            var table = getPdfPTable(map);
 
-            document.add(chunk);
+            document.add(table);
             document.close();
 
             return byteArrayOutputStream.toByteArray();
@@ -57,6 +59,32 @@ class PdfGeneratorImpl implements PdfGenerator {
             log.error("Failed to generate PDF");
             throw new RuntimeException(e);
         }
+    }
+
+    private PdfPTable getPdfPTable(Map<DayOfTheWeek, Optional<RecipeDto>> map) throws DocumentException {
+        var table = new PdfPTable(3);
+
+        table.setWidthPercentage(100);
+        table.setWidths(new float[]{0.1F, 0.7F, 4.3F});
+
+        var font = new Font(HELVETICA, 7);
+
+        map.entrySet().stream()
+                .sorted(Comparator.comparingInt(entry -> entry.getKey().ordinal()))
+                .forEach(entry -> {
+                    var dayCell = new PdfPCell(new Phrase(entry.getKey().name(), font));
+                    dayCell.setRotation(90);
+                    dayCell.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+                    dayCell.setVerticalAlignment(PdfPCell.ALIGN_MIDDLE);
+                    var nameCell = new PdfPCell(new Phrase(entry.getValue().map(RecipeDto::name).orElse("-"), font));
+                    var descriptionCell = new PdfPCell(new Phrase(entry.getValue().map(RecipeDto::description).orElse("-"), font));
+
+                    table.addCell(dayCell);
+                    table.addCell(nameCell);
+                    table.addCell(descriptionCell);
+        });
+
+        return table;
     }
 
     private Map<DayOfTheWeek, Optional<RecipeDto>> fetchRecipes(GenerateWeeklyPdfQuery query) {
